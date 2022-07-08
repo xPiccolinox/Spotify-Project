b4f0d39a2b8b258a3f2278f94952102f5ec75b78
xPiccolinox <kupkeu@gmail.com>
2022-07-08 20:28:08 +0200

    Update: Fullscreen song bar overhaul (test)

--- src/Fullscreen/FsPlayer/FsSongBar/FsSongBar.js
+++ src/Fullscreen/FsPlayer/FsSongBar/FsSongBar.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import db from '../../../data/db.json'
 
 const FsSongBar = (props) => {
-  const [audioPlayerDuration, setAudioPlayerDuration] = useState(0)
+  const [audioPlayerDuration, setAudioPlayerDuration] = useState(123)
   const [audioPlayerCurrentTime, setAudioPlayerCurrentTime] = useState(0)
   const [songRangeProgressStill, setSongRangeProgressStill] = useState(false)
   const audio = `/songs/${props.song}.mp3`
@@ -11,31 +11,107 @@ const FsSongBar = (props) => {
   let songDurationTime = '0:00'
   let thumbPosition
 
-  const onChangeHandle = () => {
-    let audioPlayer = document.getElementById('audioPlayer')
-    document.getElementById('fsSongRangeProgress').addEventListener('mousedown', () => {
-      setSongRangeProgressStill(true)
-    })
-    document.getElementById('fsSongRangeProgress').addEventListener('mouseup', () => {
-      setSongRangeProgressStill(false)
-      audioPlayer.currentTime = document.getElementById('fsSongRangeProgress').value
-    })
-    thumbPosition = document.getElementById('fsSongRangeProgress').value / document.getElementById('fsSongRangeProgress').max * 100
-    document.getElementById('fsSongRangeThumb').style.marginLeft = 'calc(' + thumbPosition + '% - ' + thumbPosition * 10 / 100 + 'px)'
-    setAudioPlayerCurrentTime(document.getElementById('fsSongRangeProgress').value)
-  }
+  if (Math.floor(audioPlayerCurrentTime % 60) < 10) songCurrentTime = `${Math.floor(audioPlayerCurrentTime / 60)}:0${Math.floor(audioPlayerCurrentTime % 60)}`
+  else songCurrentTime = `${Math.floor(audioPlayerCurrentTime / 60)}:${Math.floor(audioPlayerCurrentTime % 60)}`
+  if (Math.floor(audioPlayerDuration % 60) < 10) songDurationTime = `${Math.floor(audioPlayerDuration / 60)}:0${Math.floor(audioPlayerDuration % 60)}`
+  else songDurationTime = `${Math.floor(audioPlayerDuration / 60)}:${Math.floor(audioPlayerDuration % 60)}`
+
+  // const onChangeHandle = () => {
+  //   let audioPlayer = document.getElementById('audioPlayer')
+  //   document.getElementById('fsSongRangeProgress').addEventListener('mousedown', () => {
+  //     setSongRangeProgressStill(true)
+  //   })
+  //   document.getElementById('fsSongRangeProgress').addEventListener('mouseup', () => {
+  //     setSongRangeProgressStill(false)
+  //     audioPlayer.currentTime = document.getElementById('fsSongRangeProgress').value
+  //   })
+  //   thumbPosition = document.getElementById('fsSongRangeProgress').value / document.getElementById('fsSongRangeProgress').max * 100
+  //   document.getElementById('fsSongRangeThumb').style.marginLeft = 'calc(' + thumbPosition + '% - ' + thumbPosition * 10 / 100 + 'px)'
+  //   setAudioPlayerCurrentTime(document.getElementById('fsSongRangeProgress').value)
+  // }
 
   return(
     <div id="fsSongBar">
         <div id="fsSongCurrentTime">{songCurrentTime}</div>
         <div id="fsSongRange">
-          <input id="fsSongRangeProgress" type="range" min="0" max={audioPlayerDuration} onChange={onChangeHandle} />
-          <div id="fsSongRangeOverlay" />
-          <div id="fsSongRangeThumb" />
+          <div id="fsSongRangeOverlay">
+            <input id="fsSongRangeProgress" type="range" min="0" max={audioPlayerDuration} />
+          </div>
         </div>
+        {/* <div id="fsSongRange">
+          <div id="fsSongRangeOverlay">
+            <input id="fsSongRangeProgress" type="range" min="0" max={audioPlayerDuration} onChange={onChangeHandle} />
+          </div>
+          <div id="fsSongRangeThumb" />
+        </div> */}
         <div id="fsSongDurationTime">{songDurationTime}</div>
     </div>
   )
 }
 
 export default FsSongBar
+
+
+// /* songRange */
+// #fsSongRange {
+//   background-color: orange;
+//   /* display: flex; */
+//   width: calc(90% - 160px);
+//   height: 16px;
+//   transform: translateY(-1px);
+//   flex-shrink: 0;
+// }
+// /* songRangeThumb */
+// #fsSongRangeThumb {
+//   background-color: #ffffff;
+//   position: relative;
+//   float: left;
+//   width: 10px;
+//   height: 10px;
+//   bottom: 21px;
+//   margin-left: 0%;
+//   border-radius: 7px;
+//   filter: opacity(0);
+//   transform: translateX(0%);
+//   pointer-events: none;
+// }
+// #fsSongRangeProgress:hover ~ #fsSongRangeThumb {
+//   filter: opacity(1);
+// }
+// /* songRangeOverlay */
+// #fsSongRangeOverlay {
+//   position: relative;
+//   width: calc(100% - 8px);
+//   height: 4px;
+//   /* bottom: 10px; */
+//   border-radius: 10px;
+//   border: 4px solid #181818;
+//   /* pointer-events: none; */
+// }
+// /* songRangeProgress */
+// #fsSongRangeProgress {
+//   position: relative;
+//   float: left;
+//   appearance: none;
+//   background-color: #454545;
+//   width: calc(100% - 4px);
+//   height: 4px;
+//   bottom: -2px;
+//   border-width: 4px 4px 4px 0px;
+//   border-style: solid;
+//   border-color: #181818;
+//   border-radius: 20px;
+//   overflow: hidden;
+// }
+// #fsSongRangeProgress::-webkit-slider-thumb {
+//   appearance: none;
+//   background-color: #cccccc;
+//   border-radius: 6px;
+//   width: 4px;
+//   height: 4px;
+//   box-shadow: -1002px 0px 0px 1000px #cccccc
+// }
+// #fsSongRangeProgress:hover::-webkit-slider-thumb {
+//   background-color: #1eb955;
+//   box-shadow: -1002px 0px 0px 1000px #1eb955
+// }
