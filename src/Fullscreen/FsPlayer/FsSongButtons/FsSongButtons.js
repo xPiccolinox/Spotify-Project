b41bdaf924989873e4796f70736a1cb444c0cdb0
xPiccolinox <kupkeu@gmail.com>
2022-07-20 21:19:53 +0200

    Overhaul: Song Volume bar overhaul beginning

--- src/Fullscreen/FsPlayer/FsSongButtons/FsSongButtons.js
+++ src/Fullscreen/FsPlayer/FsSongButtons/FsSongButtons.js
@@ -1,12 +1,15 @@
 import './FsSongButtons.css'
+import { useState } from 'react'
 import db from './../../../data/db.json'
 
 const FsSongButtons = (props) => {
+  const [volume, setVolume] = useState(2)
   const like = '/icons/player/icon_like_' + db.songs[props.song].liked + '.png'
   const songShuffle = `/icons/player/icon_shuffled_${props.shuffled}.png`
   const songPause = `/icons/player/icon_paused_${props.paused}.png`
   const songRepeat = `/icons/player/icon_repeated_${props.repeated}.png`
   const fullscreenIcon = `/icons/player/icon_fullscreen_true.png`
+  const volumeIcon = `/icons/player/icon_volume_${volume}.png`
 
   return(
     <div id="fsSongButtons">
@@ -19,6 +22,14 @@ const FsSongButtons = (props) => {
         <img id="fsSongRepeat" onClick={props.repeat} src={songRepeat}></img>
       </div>
       <div id="fsSongButtonsRight">
+       <img id="fsSongVolumeButton" src={volumeIcon} />
+        <div id="fsSongVolume">
+          <input id="fsSongVolumeInput" type="range" min="0" max="100" />
+          <div id="fsSongVolumeOverlay">
+            <div id="fsSongVolumeProgress" />
+          </div>
+          <div id="fsSongVolumeThumb" />
+        </div>
         <img id="fsSongFullscreenImg" onClick={props.fullscreenChange} src={fullscreenIcon} alt="fsSongFullScreen.png" />
       </div>
     </div>
